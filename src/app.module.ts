c7b3de2ab754f5a75c07e488db67945058b83e85
FELIPE SALVADOR OLIVEIRA <25162398@edu119.local>
2026-09-09 14:22:51 -0300

    conexao com o mongo

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,14 @@ import { Module } from '@nestjs/common';
 import { createObserveModule } from '@nestjs/observe';
 import { AppController } from './app.controller.js';
 import { AppService } from './app.service.js';
+import { UsersModule } from './users/users.module.js';
+import { MongooseModule } from '@nestjs/mongoose';
 
 export const { ObserveModule, ObserveInstrument } = createObserveModule();
 
 @Module({
   imports: [
+    MongooseModule.forRoot('mongodb+srv://felipesalvador8011_db_user:YJxLWhMaJExt3vFx@carometro.t2ntbqe.mongodb.net/'),
     // Distributed tracing, auto-correlated logs, request/job metrics, error
     // telemetry, alarms, and more — out of the box. Sign up at https://observe.nestjs.com
     ObserveModule.forRoot({
@@ -14,8 +17,13 @@ export const { ObserveModule, ObserveInstrument } = createObserveModule();
       appSecret: 'YOUR_APP_SECRET',
       serviceId: 'projeto_carometro_nestjs',
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
+
+
+
+
